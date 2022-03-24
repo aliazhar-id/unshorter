de7249169ec13cf06991e6b69a3de350b93cef3a
aliazhar <aliazhar0555@gmail.com>
2022-03-24 20:28:11 +0700

    Change the module type from ES6 Modules(ESM) to CommonJS Modules(CJS) - because ESM can not be used in CJS it was decided to use CJS instead. - CJS can be used in ESM. there are several ways besides using CJS, but after thinking about it, I think CJS is the best solution for this case.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import https from 'https';
+const { get } = require('https');
 
-export default async (url) => {
+module.exports = async (url) => {
   return new Promise((resolve, reject) => {
-    https
-      .get(url, ({ rawHeaders }) => {
-        resolve(rawHeaders[rawHeaders.indexOf('Location') + 1]);
-      })
-      .on('error', () => reject());
+    get(url, ({ rawHeaders }) => {
+      resolve(rawHeaders[rawHeaders.indexOf('Location') + 1]);
+    }).on('error', () => reject());
   });
 };
