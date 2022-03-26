329abd78d4ff832d4d431d497f879bda9f4ab9fe
aliazhar <aliazhar0555@gmail.com>
2022-03-26 17:45:35 +0700

    Fix/Add Support for Short URLs with http protocol

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const { get } = require('https');
+const https = require('https');
+const http = require('http');
 
 module.exports = async (url) => {
   return new Promise((resolve, reject) => {
     const tracer = (url) => {
-        get(url, { headers: { 'User-Agent': 'FetchStream' } }, (res) => {
-          if ([301, 302, 303, 307, 308].includes(res.statusCode)) {
-            if (res.headers.location) {
-              tracer(res.headers.location);
-              return;
-            }
-          }
-          resolve(url);
-        }).on('error', () => reject());
-      };
+      let protocol = https;
 
-      tracer(url);
+      if (url.slice(0, 5) === 'http:') {
+        protocol = http;
+      }
+
+      protocol.get(url, { headers: { 'User-Agent': 'FetchStream' } }, ({statusCode, headers}) => {
+        if ([301, 302, 303, 307, 308].includes(statusCode)) {
+          if (headers.location) {
+            tracer(headers.location);
+            return;
+          }
+        }
+        resolve(url);
+      }).on('error', () => reject());
+    };
+
+    tracer(url);
   });
 };
