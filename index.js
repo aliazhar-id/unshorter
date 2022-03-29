63b38d049999cdf31e4a1280b3de9fd00f96f7c2
aliazhar <aliazhar0555@gmail.com>
2022-03-29 12:02:08 +0700

    Added a new mechanism to define protocol, change user agent, etc btw I love RegEx :D

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const http = require('http');
 
 module.exports = async (url) => {
   return new Promise((resolve, reject) => {
-    const tracer = (url) => {
-      let protocol = https;
+    try {
+      const tracer = (urls) => {
+        const protocol = urls.replace(/:?\/\/.*/g, '');
 
-      if (url.slice(0, 5) === 'http:') {
-        protocol = http;
-      }
+        if (protocol.length > 5) throw new ReferenceError('Something wrong with the protocol');
 
-      protocol.get(url, { headers: { 'User-Agent': 'FetchStream' } }, ({statusCode, headers}) => {
-        if ([301, 302, 303, 307, 308].includes(statusCode)) {
-          if (headers.location) {
-            tracer(headers.location);
-            return;
-          }
-        }
-        resolve(url);
-      }).on('error', () => reject());
-    };
+        eval(protocol)
+          .get(urls, { headers: { 'User-Agent': 'okhttp' } }, ({ statusCode, headers }) => {
+            if ([301, 302, 303, 307, 308].includes(statusCode)) {
+              if (headers.location) {
+                tracer(headers.location);
+                return;
+              }
+            }
+            resolve(urls);
+          })
+          .on('error', (err) => reject(err));
+      };
 
-    tracer(url);
+      tracer(url);
+    } catch (err) {
+      reject(err);
+    }
   });
 };
