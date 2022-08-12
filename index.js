20f1b3c6eda43f70612ab2d676b6145c270b9a8b
ipincamp <arif.lngsrn@gmail.com>
2022-08-12 09:40:45 +0700

    Choose protocol automatically
    I have tried various error handling, this is probably the most effective in my opinion. looking at your previous code, the error callback is still not caught properly, for protocols that can't run by declaring at the beginning of the code, the nested function here aims to automatically select the protocol (if http can't be used, it automatically switches to https, if both errors, then it is returned ).
    
    Thank you (^_^)

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const https = require('https');
-const http = require('http');
+const http = require("http");
+const https = require("https");
 
 module.exports = async (url, options = {}) => {
   return new Promise((resolve, reject) => {
     try {
       const tracer = (urls) => {
-        let protocol = https;
-
-        if (urls.slice(0, 5) === 'http:') {
-          protocol = http;
-        }
-
-        protocol
-          .get(urls, options, ({ statusCode, headers }) => {
-            if ([301, 302, 303, 307, 308].includes(statusCode)) {
-              if (headers.location) {
-                tracer(headers.location);
-                return;
+        function choose(protocol) {
+          protocol
+            .get(urls, options, ({ statusCode, headers }) => {
+              if ([301, 302, 303, 307, 308].includes(statusCode)) {
+                if (headers.location) {
+                  tracer(headers.location);
+                  return;
+                }
               }
-            }
-            resolve(urls);
-          })
-          .on('error', (err) => reject(err));
+              resolve(urls);
+            })
+            .on("error", (e) => reject(new Error(e?.message)));
+        }
+        try {
+          choose(http);
+        } catch {
+          choose(https);
+        }
       };
-
       tracer(url);
     } catch (err) {
-      reject(err);
+      reject(new Error(err?.message));
     }
   });
 };
